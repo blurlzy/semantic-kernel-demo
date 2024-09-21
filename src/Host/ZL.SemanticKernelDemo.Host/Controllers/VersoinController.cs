9a7bef4cfaf4618fe2f3eb2c643ee2d41f809974
Zongyi Li <blurlzy@hotmail.com>
2024-09-21 23:30:39 +1000

    added version

---
+++ src/Host/ZL.SemanticKernelDemo.Host/Controllers/VersoinController.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ZL.SemanticKernelDemo.Host.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class VersoinController : ControllerBase
+    {
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetVersion()
+        {
+            return Ok(new { version = "ZL Copilot API beta 1.0" });
+        }
+    }
+}
